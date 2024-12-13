64924607b49cefdbfff1bd87823bab85868761b1
jdelcuserobert <jdelcuserobert@okteo.fr>
2024-12-13 10:40:34 +0100

    feat: Ajout v-bind v-on

--- demo-visio-iut/src/App.vue
+++ demo-visio-iut/src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 const text = ref<string>('');
+const label = "Reset";
+
+function intialisation() {
+  text.value = "Texte initialisé";
+}
 </script>
 
 <template>
   <div>
     <InputText v-model="text"></InputText>
     {{ text }}
+
+    <Button :label="label" @click="intialisation()"></Button>
   </div>
 </template>
