3e836f9f69013f566c87c11dd349bffbc9bcd7cb
jdelcuserobert <jdelcuserobert@okteo.fr>
2024-12-13 15:32:53 +0100

    feat: Demonstration fonction vs computed vs watch

--- demo-visio-iut/src/App.vue
+++ demo-visio-iut/src/App.vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 const text = ref<string>('');
 const label = "Reset";
+const textDefault = "Texte initialisé";
+
+const isResetComputed = computed(() => {
+  console.log("Appel compute isReset");
+  return text.value === textDefault;
+})
 
 function intialisation() {
-  text.value = "Texte initialisé";
+  text.value = textDefault;
 }
+
+function isReset(): boolean {
+  console.log("Appel fonction isReset");
+  return text.value === textDefault;
+}
+
+watch(isResetComputed, (newValue, oldValue) => {
+    console.log("Changement isResetComputed ", newValue, oldValue);
+})
+
 </script>
 
 <template>
   <div>
     <InputText v-model="text"></InputText>
     {{ text }}
-
-    <Button :label="label" @click="intialisation()"></Button>
-  </div>
+  </div>s
+  <div v-if="isReset()">Initialisé</div>
+  <div v-if="isResetComputed">Initialisé compute</div>
+  <Button :label="label" @click="intialisation()"></Button>
 </template>
